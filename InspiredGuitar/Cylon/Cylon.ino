491b91bab9eea83dd21a57d9483a79bd6f4b8a26
pnolle <pnolle@web.de>
2020-07-15 23:50:59 +0200

    InspiredGuitar Cylon

--- InspiredGuitar/Cylon/Cylon.ino
+++ InspiredGuitar/Cylon/Cylon.ino
@@ -1,32 +1,43 @@
 #include <FastLED.h>
+//
+//// How many leds in your strip?
+//#define NUM_LEDS 150
+//
+//// For led chips like Neopixels, which have a data line, ground, and power, you just
+//// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+//// ground, and power), like the LPD8806, define both DATA_PIN and CLOCK_PIN
+//#define DATA_PIN 19
+//#define CLOCK_PIN 13
+//
+//// Define the array of leds
+//CRGB leds[NUM_LEDS];
 
-// How many leds in your strip?
-#define NUM_LEDS 60
 
-// For led chips like Neopixels, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806, define both DATA_PIN and CLOCK_PIN
-#define DATA_PIN 6
-#define CLOCK_PIN 13
-
-// Define the array of leds
-CRGB leds[NUM_LEDS];
+#define NUM_LEDS_PER_STRIP 150
+#define NUM_STRIPS 1
+#define NUM_RUNS 1
+#define LEDtotal (NUM_LEDS_PER_STRIP * NUM_STRIPS * NUM_RUNS)
+CRGB leds[LEDtotal];
 
 void setup() { 
 	Serial.begin(57600);
 	Serial.println("resetting");
-  	  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  	  //FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 	//LEDS.addLeds<WS2812,DATA_PIN,RGB>(leds,NUM_LEDS);
-	LEDS.setBrightness(84);
+ 
+  FastLED.addLeds<NUM_STRIPS, WS2813, 19, GRB>(leds, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NUM_STRIPS, WS2813, 17, GRB>(leds, NUM_LEDS_PER_STRIP);
+	FastLED.setBrightness(40);
 }
 
-void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+void fadeall() { for(int i = 0; i < LEDtotal; i++) { leds[i].nscale8(250); } }
 
 void loop() { 
 	static uint8_t hue = 0;
 	Serial.print("x");
+     delay(500);
 	// First slide the led in one direction
-	for(int i = 0; i < NUM_LEDS; i++) {
+	for(int i = 0; i < LEDtotal; i++) {
 		// Set the i'th led to red 
 		leds[i] = CHSV(0, 255, 255);
 		// Show the leds
@@ -40,7 +51,7 @@ void loop() {
 	Serial.print("x");
 
 	// Now go in the other direction.  
-	for(int i = (NUM_LEDS)-1; i >= 0; i--) {
+	for(int i = (LEDtotal)-1; i >= 0; i--) {
 		// Set the i'th led to red 
 		leds[i] = CHSV(hue++, 255, 255);
 		// Show the leds
