41e80ea38e61496555e3b88a83a95771f408cc0f
pnolle <pnolle@web.de>
2023-09-10 22:28:44 +0200

    palette cleanup

--- Snippetu/SnipSign_ambient/SnipSignAmbient/SnipSignAmbient.ino
+++ Snippetu/SnipSign_ambient/SnipSignAmbient/SnipSignAmbient.ino
@@ -300,33 +300,8 @@ void coolLikeIncandescent(CRGB &c, uint8_t phase)
   c.b = qsub8(c.b, cooling * 2);
 }
 
-// Dummy palette: Cylon
+// Cylon mode
 const TProgmemRGBPalette16 Cylon FL_PROGMEM = {};
-
-// A mostly red palette with green accents and white trim.
-// "CRGB::Gray" is used as white to keep the brightness more uniform.
-const TProgmemRGBPalette16 RedGreenWhite_p FL_PROGMEM =
-    {CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red,
-     CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red,
-     CRGB::Red, CRGB::Red, CRGB::Gray, CRGB::Gray,
-     CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green};
-
-// A mostly (dark) green palette with red berries.
-#define Holly_Green 0x00580c
-#define Holly_Red 0xB00402
-const TProgmemRGBPalette16 Holly_p FL_PROGMEM =
-    {Holly_Green, Holly_Green, Holly_Green, Holly_Green,
-     Holly_Green, Holly_Green, Holly_Green, Holly_Green,
-     Holly_Green, Holly_Green, Holly_Green, Holly_Green,
-     Holly_Green, Holly_Green, Holly_Green, Holly_Red};
-
-// A red and white striped palette
-// "CRGB::Gray" is used as white to keep the brightness more uniform.
-const TProgmemRGBPalette16 RedWhite_p FL_PROGMEM =
-    {CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red,
-     CRGB::Gray, CRGB::Gray, CRGB::Gray, CRGB::Gray,
-     CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red,
-     CRGB::Gray, CRGB::Gray, CRGB::Gray, CRGB::Gray};
 
 // Warm White
 const TProgmemRGBPalette16 WarmWhite_p FL_PROGMEM =
@@ -343,35 +318,12 @@ const TProgmemRGBPalette16 BlueWhite_p FL_PROGMEM =
      CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue,
      CRGB::Blue, CRGB::Gray, CRGB::Gray, CRGB::Gray};
 
-// A pure "fairy light" palette with some brightness variations
-#define HALFFAIRY ((CRGB::FairyLight & 0xFEFEFE) / 2)
-#define QUARTERFAIRY ((CRGB::FairyLight & 0xFCFCFC) / 4)
-const TProgmemRGBPalette16 FairyLight_p FL_PROGMEM =
-    {CRGB::FairyLight, CRGB::FairyLight, CRGB::FairyLight, CRGB::FairyLight,
-     HALFFAIRY, HALFFAIRY, CRGB::FairyLight, CRGB::FairyLight,
-     QUARTERFAIRY, QUARTERFAIRY, CRGB::FairyLight, CRGB::FairyLight,
-     CRGB::FairyLight, CRGB::FairyLight, CRGB::FairyLight, CRGB::FairyLight};
-
 // A palette of soft snowflakes with the occasional bright one
 const TProgmemRGBPalette16 Snow_p FL_PROGMEM =
     {0x304048, 0x304048, 0x304048, 0x304048,
      0x304048, 0x304048, 0x304048, 0x304048,
      0x304048, 0x304048, 0x304048, 0x304048,
      0x304048, 0x304048, 0x304048, 0xE0F0FF};
-
-// A palette reminiscent of large 'old-school' C9-size tree lights
-// in the five classic colors: red, orange, green, blue, and white.
-#define C9_Red 0xB80400
-#define C9_Orange 0x902C02
-#define C9_Green 0x046002
-#define C9_Blue 0x070758
-#define C9_White 0x606820
-const TProgmemRGBPalette16 RetroC9_p FL_PROGMEM =
-    {C9_Red, C9_Orange, C9_Red, C9_Orange,
-     C9_Orange, C9_Red, C9_Orange, C9_Red,
-     C9_Green, C9_Green, C9_Green, C9_Green,
-     C9_Blue, C9_Blue, C9_Blue,
-     C9_White};
 
 // A cold, icy pale blue palette
 #define Ice_Blue1 0x0C1040
@@ -388,16 +340,9 @@ const TProgmemRGBPalette16 Ice_p FL_PROGMEM =
 // palettes are used, and in what order.
 const TProgmemRGBPalette16 *ActivePaletteList[] = {
     &Cylon,
-    &RetroC9_p,
     &BlueWhite_p,
     &WarmWhite_p,
-    &RainbowColors_p,
-    &FairyLight_p,
-    &RedGreenWhite_p,
-    &PartyColors_p,
-    &RedWhite_p,
     &Snow_p,
-    &Holly_p,
     &Ice_p};
 
 // Advance to the next color palette in the list (above).
@@ -441,13 +386,12 @@ void loop()
   // reading input: program change btn
   readPrgChgBtn();
 
+  Serial.println(*gTargetPalette);
   if (gTargetPalette == Cylon) {
-    Serial.print("c");
     cylonUpdateLedFrame();
   }
   else {
-    Serial.print("p");
-    nblendPaletteTowardPalette(gCurrentPalette, gTargetPalette, 12);
+    nblendPaletteTowardPalette(gCurrentPalette, gTargetPalette, 60);
     drawTwinkles(leds);
   }
   FastLED.show();
